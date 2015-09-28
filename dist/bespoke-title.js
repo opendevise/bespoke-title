345abfd6a6f97f7deed4416c6af52bfe817b9eeb
Dan Allen <dan.j.allen@gmail.com>
2015-09-28 00:10:15 -0600

    make it so

---
+++ dist/bespoke-title.js
@@ -0,0 +1,28 @@
+/*!
+ * bespoke-title v1.0.0-dev
+ *
+ * Copyright 2015, Dan Allen
+ * This content is released under the MIT license
+ */
+
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g=(g.bespoke||(g.bespoke = {}));g=(g.plugins||(g.plugins = {}));g.title = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports = function() {
+  return function(deck) {
+    var originalTitle = document.title,
+      presentationTitle = originalTitle,
+      getSlideTitle = function(slide) {
+        var title = slide.getAttribute('data-title');
+        if (title === null && (title = slide.querySelector('h1,h2')) !== null) title = title.textContent;
+        return title === null || title.length === 0 ? null : title;
+      },
+      onActivate = function(e) {
+        var slideTitle = getSlideTitle(e.slide);
+        document.title = slideTitle !== null ? slideTitle + ' — ' + presentationTitle : presentationTitle;
+      };
+    deck.on('destroy', function() { document.title = originalTitle; });
+    deck.on('activate', onActivate);
+  };
+};
+
+},{}]},{},[1])(1)
+});
---
+++ test/spec/bespoke-titleSpec.js
@@ -0,0 +1,68 @@
+Function.prototype.bind = Function.prototype.bind || require('function-bind');
+
+var bespoke = require('bespoke'),
+  title = require('../../lib/bespoke-title.js');
+
+describe('bespoke-title', function() {
+  var deck,
+    setup = function() {
+      document.title = 'bespoke-title tests';
+    },
+    createDeck = function() {
+      var parent = document.createElement('article');
+      for (var i = 1; i <= 5; i++) {
+        var section = document.createElement('section');
+        var slideTitle = document.createElement('h2');
+        slideTitle.appendChild(document.createTextNode('Slide ' + i));
+        section.appendChild(slideTitle);
+        if (i === 1) {
+          section.setAttribute('data-title', '');
+        }
+        else if (i % 2 !== 0) {
+          section.setAttribute('data-title', 'Override Slide ' + i);
+        }
+        parent.appendChild(section);
+      }
+
+      document.body.appendChild(parent);
+
+      deck = bespoke.from(parent, [
+        title()
+      ]);
+    },
+    destroyDeck = function() {
+      deck.fire('destroy');
+      if (deck.parent.parentNode) deck.parent.parentNode.removeChild(deck.parent);
+      deck = null;
+    };
+
+  beforeAll(setup);
+
+  describe('restores title', function() {
+    it('should restore original title when deck is destroyed', function() {
+      expect(document.title).toBe('bespoke-title tests');
+      createDeck();
+      deck.next();
+      expect(document.title).not.toBe('bespoke-title tests');
+      destroyDeck();
+      expect(document.title).toBe('bespoke-title tests');
+    });
+  });
+
+  describe('slide change', function() {
+    beforeEach(createDeck);
+    afterEach(destroyDeck);
+
+    it('should set title to h2 text when slide without data-title attribute is activated', function() {
+      expect(document.title).toBe('bespoke-title tests');
+      deck.next();
+      expect(document.title).toBe('Slide 2 — bespoke-title tests');
+    });
+
+    it('should set title to data-title when slide with data-title attribute is activated', function() {
+      expect(document.title).toBe('bespoke-title tests');
+      deck.slide(2);
+      expect(document.title).toBe('Override Slide 3 — bespoke-title tests');
+    });
+  });
+});
